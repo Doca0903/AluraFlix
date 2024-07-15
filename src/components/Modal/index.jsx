1e6960b199e2fd35e10b70ca8c9681682983681c
Aman <160797507+Doca0903@users.noreply.github.com>
2024-07-15 13:52:12 -0300

    Add files via upload

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,63 +1,208 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import EditarCard from './EditarCard';
+import { saveVideo } from '../../Metodo POST';
+import { updateVideo } from '../../Metodo PUT';
 
-const Overlay = styled.div`
-  background-color: rgba(3, 18, 47, 0.6);
-  position: fixed;
-  top: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
+const CardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
 
-const ModalDialog = styled.dialog`
-  width: 900px;
-  padding-bottom: 5rem;
-  border: 5px solid #6BD1FF;
-  background-color: #03122F;
-  border-radius: 10px;
+const Opcao = styled.option`
+  color: black;
+`;
+
+const CardTitle = styled.h1`
+  color: #2271d1;
+  font-weight: 700;
+  font-size: 32px;
+`;
+
+const CardLabel = styled.label`
   color: white;
-  position: absolute;
-  top: 600px;
-  justify-content: center;
-`;
-
-const CloseButton = styled.button`
-  border: none;
-  background: none;
-  cursor: pointer;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-`;
-
-const CloseButtonImg = styled.img`
-  width: 40px;
   display: block;
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 10px;
 `;
 
-function ModalZoom({ isOpen, onClose, videoToEdit, onSave }) {
-  return (
-    <>
-      {isOpen && (
-        <>
-          <Overlay />
-          <ModalDialog open={true}>
-            <CloseButton onClick={onClose}>
-              <CloseButtonImg src="/imagens/X-cancel.png" alt="Botão Fechar" />
-            </CloseButton>
-            <EditarCard 
-              titulo="EDITAR VIDEO:" 
-              subtitulo="Atualize as informações do vídeo"
-              videoToEdit={videoToEdit}
-              //onSave={onSave}
-            />
-            <form method="dialog"></form>
-          </ModalDialog>
-        </>
-      )}
-    </>
-  );
-}
+const CardInput = styled.input`
+  border: 2px solid #2271d1;
+  border-radius: 10px;
+  background-color: transparent;
+  margin-bottom: 30px;
+  padding: 15px;
+  width: 500px;
+  color: white;
+  font-size: 20px;
+`;
 
-export default ModalZoom;
+const CardSelect = styled.select`
+  border: 2px solid #2271d1;
+  border-radius: 10px;
+  background-color: transparent;
+  margin-bottom: 30px;
+  padding: 15px;
+  width: 531px;
+  color: white;
+  font-size: 20px;
+`;
+
+const CardTextarea = styled.textarea`
+  border: 2px solid #2271d1;
+  border-radius: 10px;
+  background-color: transparent;
+  margin-bottom: 30px;
+  padding: 15px;
+  width: 500px;
+  color: white;
+  font-size: 20px;
+`;
+
+const CardButton = styled.button`
+  border: 2px solid white;
+  color: white;
+  font-size: 18px;
+  width: 180px;
+  padding: 10px;
+  background-color: transparent;
+  border-radius: 10px;
+
+  &:hover {
+    border: 2px solid rgba(34, 113, 209, 1);
+    color: rgba(34, 113, 209, 1);
+    background-color: rgba(0, 0, 0, 0.9);
+  }
+`;
+
+const CardButtonSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Subtitulo = styled.p`
+  color: white;
+  font-size: 18px;
+`;
+
+const EditarCard = ({ titulo, subtitulo, videoToEdit }) => {
+  const [valor, setValor] = useState('');
+  const [valorImg, setValorImg] = useState('');
+  const [valorLink, setValorLink] = useState('');
+  const [valorCampo, setValorCampo] = useState('');
+  const [valorOp, setValorOp] = useState('');
+
+  useEffect(() => {
+    if (videoToEdit) {
+      setValor(videoToEdit.titulo);
+      setValorImg(videoToEdit.imagem);
+      setValorLink(videoToEdit.video);
+      setValorCampo(videoToEdit.descricao);
+      setValorOp(videoToEdit.categoria);
+    }
+  }, [videoToEdit]);
+
+  const aoDigitado = (evento) => {
+    setValor(evento.target.value);
+  };
+
+  const aoDigitadoImg = (evento) => {
+    setValorImg(evento.target.value);
+  };
+
+  const aoDigitadoLink = (evento) => {
+    setValorLink(evento.target.value);
+  };
+
+  const aoDigitadoCampo = (evento) => {
+    setValorCampo(evento.target.value);
+  };
+
+  const aoDigitadoOp = (evento) => {
+    setValorOp(evento.target.value);
+  };
+
+  const aoSalvar = async (evento) => {
+    evento.preventDefault();
+  
+    if (videoToEdit) {
+      const videoAtualizado = {
+        id: videoToEdit.id,
+        titulo: valor,
+        categoria: valorOp,
+        imagem: valorImg,
+        video: valorLink,
+        descricao: valorCampo,
+      };
+  
+      const sucesso = await updateVideo(videoToEdit.id, videoAtualizado); 
+  
+     // if (sucesso) {
+        //onSave();
+      //}
+    } else {
+      const novoVideo = {
+        titulo: valor,
+        categoria: valorOp,
+        imagem: valorImg,
+        video: valorLink,
+        descricao: valorCampo,
+      };
+  
+      const sucesso = await saveVideo(novoVideo);
+  
+      //if (sucesso) {
+       // onSave(); 
+      //}
+    }
+  
+   
+    setValor('');
+    setValorImg('');
+    setValorLink('');
+    setValorCampo('');
+    setValorOp('');
+  };
+
+  return (
+    <CardContainer>
+      <CardTitle>{titulo}</CardTitle>
+      <Subtitulo>{subtitulo}</Subtitulo>
+      <form onSubmit={aoSalvar}>
+        <div>
+          <CardLabel htmlFor="titulo">Título</CardLabel>
+          <CardInput value={valor} onChange={aoDigitado} type="text" id="titulo" name="titulo" required />
+        </div>
+        <div>
+          <CardLabel htmlFor="categoria">Categoria</CardLabel>
+          <CardSelect value={valorOp} onChange={aoDigitadoOp} id="categoria" name="categoria" required>
+            <Opcao value="">Selecione uma categoria</Opcao>
+            <Opcao value="Front End">Front End</Opcao>
+            <Opcao value="Back End">Back End</Opcao>
+            <Opcao value="Inovação">Inovação</Opcao>
+            <Opcao value="Gestão">Gestão</Opcao>
+          </CardSelect>
+        </div>
+        <div>
+          <CardLabel htmlFor="imagem">Imagem</CardLabel>
+          <CardInput value={valorImg} onChange={aoDigitadoImg} type="url" id="imagem" name="imagem" />
+        </div>
+        <div>
+          <CardLabel htmlFor="video">Vídeo</CardLabel>
+          <CardInput value={valorLink} onChange={aoDigitadoLink} type="url" id="video" name="video" placeholder="https://www.youtube.com/watch?v=..." />
+        </div>
+        <div>
+          <CardLabel htmlFor="descricao">Descrição</CardLabel>
+          <CardTextarea value={valorCampo} onChange={aoDigitadoCampo} id="descricao" name="descricao" rows="4" required></CardTextarea>
+        </div>
+        <CardButtonSection>
+          <CardButton type="submit">Salvar</CardButton>
+          <CardButton type="reset">Limpar</CardButton>
+        </CardButtonSection>
+      </form>
+    </CardContainer>
+  );
+};
+
+export default EditarCard;
